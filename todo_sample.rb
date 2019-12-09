c34dc446bfd7ad5396199437b9eae1792d3bfd5d
DoroYoshi <yoshimatsu44dy@gmail.com>
2019-12-09 18:11:33 +0900

    一通り完成はしたが、 いまいち納得いかない。
    これだとTaskクラスの存在意義はあるのか？

--- todo_sample.rb
+++ todo_sample.rb
@@ -1,26 +1,4 @@
-class ToDo
-  attr_accessor :add, :index, :destroy
-  
-  def add
-    todoes = []
-    puts todoes << "タスクNo.#{@id} => タスク名:#{@name} , 詳細:#{@description}"
-    puts "タスクを追加しました"
-    puts todoes
-  end
-
-  def index
-    @todoes.each do |todo|
-      puts todo
-    end
-  end
-
-  def destroy
-    todo = @todoes.find(params[:id])
-    todo.delete
-  end
-end
-
-class Task < ToDo
+class Task
   def initialize(**params)
     @id = params[:id]
     @name = params[:name]
@@ -28,15 +6,36 @@ class Task < ToDo
   end
 end
 
-task1 = Task.new(id: 1, name: "洗濯", description: "午前中に干し終える")
-puts task1.add
-task2 = Task.new(id: 2, name: "買い物", description: "午後１時にスーパーに行く")
-puts task2.add
-task3 = Task.new(id: 3, name: "夕食準備", description: "午後５時より開始する")
-puts task3.add
-task4 = Task.new(id: 4, name: "勉強", description: "午後３時より１時間勉強する")
-puts task4.add
-task5 = Task.new(id: 5, name: "昼寝", description: "午後２時半から20分間昼寝する")
-puts task5.add
+class Todo
 
-puts task1.index
+  @@task_array = []
+
+  def self.add(**params)
+    @task = { id: params[:id], name: params[:name], description: params[:description] }
+    @tasks = @@task_array << @task
+    puts "タスクNo.#{params[:id]} #{params[:name]}:#{params[:description]}  を追加しました。"
+  end
+
+  def self.index
+    puts "＜＜タスクの一覧＞＞"
+    @tasks.each do |task|
+      puts "タスクNo.#{task[:id]} #{task[:name]}:#{task[:description]}"
+    end
+  end
+
+  def self.destroy(id)
+    delete_task = @tasks[id-1]
+    @tasks = @tasks - [delete_task]
+    puts "タスクNo.#{delete_task[:id]} #{delete_task[:name]}:#{delete_task[:description]}  を削除しました。"
+  end
+end
+
+Todo.add(id: 1, name: "洗濯", description: "午前に干し終える")
+Todo.add(id: 2, name: "買物", description: "午後１時に行く")
+Todo.add(id: 3, name: "夕食", description: "午後５時に食べる")
+Todo.add(id: 4, name: "勉強", description: "午後３時に勉強する")
+Todo.add(id: 5, name: "昼寝", description: "午後２時に昼寝する")
+
+Todo.index
+Todo.destroy(3)
+Todo.index
