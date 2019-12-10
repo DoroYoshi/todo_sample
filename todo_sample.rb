5bf4096c4e4ded5502daa2a10766f4484e769fa6
DoroYoshi <yoshimatsu44dy@gmail.com>
2019-12-10 19:53:23 +0900

    修正。 idの取り扱いがまだ未完成。

--- todo_sample.rb
+++ todo_sample.rb
@@ -1,4 +1,6 @@
 class Task
+  attr_accessor :id, :name, :description
+
   def initialize(**params)
     @id = params[:id]
     @name = params[:name]
@@ -7,35 +9,38 @@ class Task
 end
 
 class Todo
-
+  
   @@task_array = []
 
-  def self.add(**params)
-    @task = { id: params[:id], name: params[:name], description: params[:description] }
-    @tasks = @@task_array << @task
-    puts "タスクNo.#{params[:id]} #{params[:name]}:#{params[:description]}  を追加しました。"
+  def add(task)
+    task_hash = { id: task.id, name: task.name, description: task.description }
+    @tasks = @@task_array << task_hash
+    puts "＜タスクの追加＞"
+    puts "タスクNo.#{task.id} #{task.name}:#{task.description}  を追加しました。"
   end
 
-  def self.index
+  def index
     puts "＜＜タスクの一覧＞＞"
     @tasks.each do |task|
       puts "タスクNo.#{task[:id]} #{task[:name]}:#{task[:description]}"
     end
   end
 
-  def self.destroy(id)
-    delete_task = @tasks[id-1]
+  def destroy(**params)
+    delete_task = @tasks[params[:id]-1]
     @tasks = @tasks - [delete_task]
+    puts "＜タスクの削除＞"
     puts "タスクNo.#{delete_task[:id]} #{delete_task[:name]}:#{delete_task[:description]}  を削除しました。"
   end
 end
 
-Todo.add(id: 1, name: "洗濯", description: "午前に干し終える")
-Todo.add(id: 2, name: "買物", description: "午後１時に行く")
-Todo.add(id: 3, name: "夕食", description: "午後５時に食べる")
-Todo.add(id: 4, name: "勉強", description: "午後３時に勉強する")
-Todo.add(id: 5, name: "昼寝", description: "午後２時に昼寝する")
-
-Todo.index
-Todo.destroy(3)
-Todo.index
+task1 = Task.new(id: 1, name: "洗濯", description: "午前に干し終える")
+task2 = Task.new(id: 2, name: "買物", description: "午後１時に行く")
+task3 = Task.new(id: 3, name: "夕食", description: "午後５時に食べる")
+my_todo = Todo.new
+my_todo.add(task1)
+my_todo.add(task2)
+my_todo.add(task3)
+my_todo.index
+my_todo.destroy(id: 1)
+my_todo.index
